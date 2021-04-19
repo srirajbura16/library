efdc9378c1a17b80634b12f4a9a6b8b5f7509c86
Sriraj Bura <sriraj.bura16@gmail.com>
2021-04-19 08:26:22 -0400

    implemented multiple ways to exit popup modal

--- library.js
+++ library.js
@@ -91,3 +91,9 @@ addBookBtn.addEventListener('click', function(){
 closeBtn.addEventListener('click', function(){
   modal.style.display = 'none'
 })
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
